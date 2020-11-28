5d469af99f60bb2c30ce1dff44e6dce4acf3be05
Agustin <decarloagustin@gmail.com>
2020-11-28 15:26:19 -0300

    Cargo nuevo usuario en tabla de usuario

--- src/app.py
+++ src/app.py
@@ -1,54 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify,request #Contiene toda la informacion del cliente
-from forms import SignupForm
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 
 
 #se importan librerias para pagina web
 from flask import render_template, redirect, url_for, session
 
+from forms import SignupForm #PostForm, LoginForm
+from models import usuario, Album
 
-#from flask_wtf import FlaskForm
-#from wtforms import Form, BooleanField, StringField, PasswordField, validators, SubmitField
-#from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+import sqlobject as SO
 
 #from flask_mysqldb import MySQL
-import bcrypt 
+#import bcrypt 
 
 
 app = Flask(__name__, template_folder = '../templates', static_folder='../static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://invitado:invitado@localhost/app_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-
-#Semilla para encriptamiento
-semilla = bcrypt.gensalt()
-
-class Task(db.Model):
-    id        = db.Column(db.Integer    , primary_key=True)
-    username  = db.Column(db.String(26) , unique=True, nullable=False)
-    email     = db.Column(db.String(100), unique=True, nullable=False)
-    password  = db.Column(db.String(60) , nullable=False)
-#    def __init__(self, title, description):
-#        self.title = title
-#        self.description = description
-
-db.create_all()
-
-class TaskSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'username', 'email', 'password')
-
-
-task_schema = TaskSchema()
-tasks_schema = TaskSchema(many=True)
-
-
 
 
 @app.route("/")
@@ -61,18 +30,18 @@ def index():
 #Defino la ruta principal
 @app.route('/registro/', methods=['GET', 'POST'])
 def log():
-#    if request.method == 'POST':
-#      name = request.form['username']
-#      email = request.form['password']
-#      password = request.form['email']
   form = SignupForm()
   if form.validate_on_submit():
       name = form.username.data
       email = form.email.data
-      password = form.password.data
+      passwor = form.password.data
       print(name)
       print(email)
-      print(password)
+      print(passwor)
+
+      usuario(username = name, email = email, passwo = usuario.set_password(passwor))
+      
+
   return render_template('registro.html', form = form)
 
 
@@ -81,9 +50,23 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+
+
 if __name__ == "__main__":
+
+    connection = SO.connectionForURI("mysql://invitado:invitado@localhost/app_db")
+    SO.sqlhub.processConnection = connection
+    # connection.debug = True
+
+    # borro las tablas si ya existian
+    
+    usuario.dropTable(ifExists=True)
+    Album.dropTable(ifExists=True)
+
+    # creo las tablas
+    Album.createTable()
+    usuario.createTable()
+
     app.run(debug=True)
 
-
-#Tengo que hacer algo si los datos no son validos
-#Tengo que agregar vistas de una misma pagina
--- src/models.py
+++ src/models.py
@@ -5,35 +5,23 @@ AUTOR: Juanjo
 FECHA DE CREACIÓN: 15/02/2019
 
 """
-
-from flask_login import UserMixin
+import sqlobject as SO
+from flask_login import UserMixin #?
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(UserMixin):
+class usuario(SO.SQLObject):
+    username  = SO.StringCol(length = 25, default=None)
+    email     = SO.StringCol(length = 100, default=None)
+    passwo  = SO.StringCol(length = 100,  default=None)
+    albums = SO.ForeignKey('Album', default=None)
 
-    def __init__(self, id, name, email, password, is_admin=False):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = generate_password_hash(password)
-        self.is_admin = is_admin
+    def set_password(password):
+        return generate_password_hash(password)
 
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
+    def check_password(password_):
+        return check_password_hash(password_, password)    
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.email)
-
-
-users = []
-
-
-def get_user(email):
-    for user in users:
-        if user.email == email:
-            return user
-    return None
+class Album(SO.SQLObject):
+    title = SO.StringCol(length=160, varchar=True)
+    #usuario = SO.RelatedJoin('usuario')
